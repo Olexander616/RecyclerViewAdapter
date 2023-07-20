835661a1b923c8715f4b415dbb95dccfca95fb75
Olexander616 <109907687+Olexander616@users.noreply.github.com>
2023-07-20 22:47:47 +0300

    Add items

--- app/src/main/java/com/example/recyclerviewadapter/MainActivity.kt
+++ app/src/main/java/com/example/recyclerviewadapter/MainActivity.kt
@@ -8,31 +8,34 @@ import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var recyclerView : RecyclerView
+    private lateinit var adapter : ItemAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-    val itemList = listOf(
-        ItemData(R.drawable.ic_android_black_24dp, "Ann 1"),
-        ItemData(R.drawable.baseline_aod_24, "Igor 2"),
-        ItemData(R.drawable.baseline_aod_24, "Kate 3")
-        // Add more items as needed
-    )
+//    val itemList = listOf(
+//        ItemData(R.drawable.ic_android_black_24dp, "Ann 1"),
+//        ItemData(R.drawable.baseline_aod_24, "Igor 2"),
+//        ItemData(R.drawable.baseline_aod_24, "Kate 3")
+//        // Add more items as needed
+//    )
 
-    //val itemList = generateListOf100Elements()
+    val itemList = generateListOf100Elements()
 
     recyclerView = findViewById(R.id.my_recycleView)
-    val adapter = ItemAdapter(this, itemList)
+    adapter = ItemAdapter(this, itemList)
     recyclerView.layoutManager = LinearLayoutManager(this)
     recyclerView.adapter = adapter
 }
 }
 
-//fun generateListOf100Elements(): List<ItemData> {
-//    val itemList = mutableListOf<Int>()
-//    for (i in 1..100)
-//        itemList.add(i)
-//    }
-//    return itemList
-//}
+fun generateListOf100Elements(): List<ItemData> {
+    val itemList = mutableListOf<ItemData>()
+    for (i in 1..100){
+        val avatarResID = if (i%2==0) R.drawable.ic_android_black_24dp else R.drawable.baseline_aod_24
+        val name = "Name$i"
+        itemList.add(ItemData(avatarResID,name))
+    }
+    return itemList
+}
